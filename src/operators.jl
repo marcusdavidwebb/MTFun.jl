2f1f5e8d77a22d0a514fd1830205a8be2ec92bb1
Marcus Webb <marcusdavidwebb@gmail.com>
2021-08-24 15:27:27 +0100

    Added Weideman space and tests for operators

--- src/operators.jl
+++ src/operators.jl
@@ -1,5 +1,11 @@
-# Multiplication
-Multiplication(f::Fun{MT},sp::MT) where MT <: MalmquistTakenaka = ConcreteMultiplication(f,sp)
+# Multiplication operators MT x MT to MT
+function Multiplication(f::Fun{MT},sp::MT) where MT <: MalmquistTakenaka 
+    if spacescompatible(space(f),sp)
+        ConcreteMultiplication(f,sp)
+    else
+        error("Failure to build multiplication operator. Spaces not compatible.")
+    end
+end
 
 space(M::ConcreteMultiplication{MT,MT,T}) where {MT <: MalmquistTakenaka, T} = M.space
 rangespace(M::ConcreteMultiplication{MT,MT,T}) where {MT <: MalmquistTakenaka,T} = domainspace(M)
@@ -18,12 +24,14 @@ function getindex(M::ConcreteMultiplication{MT,MT},k::Integer,j::Integer) where 
     biinf_ind2 = k - j - 1
     ind2 = (biinf_ind2 ≥ 0) ? 1 + 2biinf_ind2 : -2biinf_ind2
 
+    scaling = 1/sqrt(4*abs(imag(space(M).λ))*π)
+
     val = zero(eltype(coefficients(M.f)))
-    if ind1 ≤ length(coefficients(M.f)) 
-        val += coefficients(M.f)[ind1] / sqrt(2π)
+    if ind1 ≤ length(coefficients(M.f))
+        val += coefficients(M.f)[ind1]*scaling
     end
     if ind2 ≤ length(coefficients(M.f))
-        val -= 1im*coefficients(M.f)[ind2] / sqrt(2π)
+        val -= 1im*coefficients(M.f)[ind2]*scaling
     end
     return val
 end
@@ -34,7 +42,47 @@ function bandwidths(M::ConcreteMultiplication{MT,MT}) where MT <: MalmquistTaken
     return n,n
 end
 
-## Derivative
+# Multiplication operators W x MT to MT and W x W to W (same matrix)
+function Multiplication(f::Fun{W},sp::S) where {W<:Weideman, S <: Union{Weideman,MalmquistTakenaka}}  
+    if spacescompatible(space(f),sp)
+        ConcreteMultiplication(f,sp)
+    else
+        error("Failure to build multiplication operator. Spaces not compatible.")
+    end
+end
+
+space(M::ConcreteMultiplication{W,S,T}) where {W<:Weideman, S <: Union{Weideman,MalmquistTakenaka}, T} = M.space
+rangespace(M::ConcreteMultiplication{W,S,T}) where {W<:Weideman, S <: Union{Weideman,MalmquistTakenaka},T} = domainspace(M)
+
+Base.stride(M::ConcreteMultiplication{U,V}) where {U<:Weideman,V<:Union{Weideman,MalmquistTakenaka}} =
+    stride(M.f)
+
+function getindex(M::ConcreteMultiplication{W,S},k::Integer,j::Integer) where {W<:Weideman, S <: Union{Weideman,MalmquistTakenaka}} 
+    # convert k,j to biinfinite indices
+    k=iseven(k) ? -k÷2 : (k-1)÷2
+    j=iseven(j) ? -j÷2 : (j-1)÷2
+    
+    # convert k - j to original indices
+    biinf_ind1 = k - j
+    ind = (biinf_ind1 ≥ 0) ? 1 + 2biinf_ind1 : -2biinf_ind1
+
+    val = zero(eltype(coefficients(M.f)))
+    if ind ≤ length(coefficients(M.f)) 
+        val += coefficients(M.f)[ind]
+    end
+    return val
+end
+
+function bandwidths(M::ConcreteMultiplication{W,S}) where {W <: Weideman, S <: Union{Weideman,MalmquistTakenaka}}
+    n = length(coefficients(M.f))
+    n = n + mod(n,2) - 1
+    return n,n
+end
+
+
+
+
+## Derivative MT to MT
 
 function Derivative(sp::MT,order::Integer) where MT <: MalmquistTakenaka
     if order == 1
@@ -53,10 +101,8 @@ function getindex(D::ConcreteDerivative{MalmquistTakenaka{T},K,KK},k::Integer,j:
     if k == j
         ((-one(T))^(k+1))im * (k-mod(k+1,2)) /(2*imag(space(D).λ))
     elseif k == j+2
-        #(-one(T))^(j+1) * 
         div(j+1,2) /(2*imag(space(D).λ))
     elseif j == k+2
-        #(-one(T))^k * 
         -div(k+1,2) /(2*imag(space(D).λ))
     else
         zero(T)
--- test/runtests.jl
+++ test/runtests.jl
@@ -33,4 +33,82 @@ using ApproxFun
     itrans = MTFun.plan_itransform(MT,v)
     @test v ≈ itrans*(trans*v)
     @test v ≈ trans*(itrans*v)
+
+    W = Weideman(λ)
+    φ = (x,n) -> (-1.0im)^n * (λ - x)^n / (conj(λ) - x)^n
+    @test coefficients(Fun(x->φ(x,0),W,12)) ≈ [1;zeros(11)] 
+    @test coefficients(Fun(x->φ(x,5),W,12)) ≈ [zeros(10);1;0]
+    @test coefficients(Fun(x->φ(x,0),W,11)) ≈ [1;zeros(10)] 
+    @test coefficients(Fun(x->φ(x,5),W,11)) ≈ [zeros(10);1]
+    
+    f = x -> (1.0-2im)/(1+x^2)
+    F = Fun(f,W)
+    x = 0.78
+    @test F(x) ≈ f(x)
+
+    f = x -> exp(-10x^2)
+    F = Fun(f,W)
+    x = 0.78
+    @test real(F(x)) ≈ f(x)
+
+    vals = φ.(points(W,10),0)
+    cfs = [1.0+0.0im;zeros(9)]
+    trans = MTFun.plan_transform(W,vals)
+    itrans = MTFun.plan_itransform(W,cfs)
+    @test cfs ≈ trans*vals
+    @test vals ≈ itrans*cfs
+
+    v = [0.83+0.0im;0.78;-1.13;0.65;1.11]
+    trans = MTFun.plan_transform(W,v)
+    itrans = MTFun.plan_itransform(W,v)
+    @test v ≈ itrans*(trans*v)
+    @test v ≈ trans*(itrans*v)
+
+end
+
+@testset "Operators" begin
+
+    λ = -0.7+0.9im
+    MT = MalmquistTakenaka(λ)
+
+    f = x -> (1.0-2im)/(1+x^2)
+    fprime = x -> -(1.0 - 2im)*2x/(1+x^2)^2
+    F = Fun(f,MT)
+    Fprime = differentiate(F)
+    x = 0.78
+    @test Fprime(x) ≈ fprime(x)
+    
+    f = x -> exp(-10x^2)
+    fprime = x -> -20*x*exp(-10x^2)
+    F = Fun(f,MT)
+    Fprime = differentiate(F)
+    x = 0.78
+    @test Fprime(x) ≈ fprime(x)
+
+    g = x -> sech(x)^2
+    G = Fun(g,MT)
+    M = Multiplication(G,MT)
+    GF = M*F
+    x = 0.78
+    @test GF(x) ≈ g(x) * f(x)
+
+
+    W = Weideman(λ)
+
+    f = x -> (1.0-2im)/(1+x^2)
+    F = Fun(f,MT)
+    g = x -> sech(x)^2
+    G = Fun(g,W)
+    
+    M = Multiplication(G,MT)
+    GF = M*F
+    x = 0.78
+    @test GF(x) ≈ g(x) * f(x)
+
+    F = Fun(f,W)
+    M = Multiplication(G,W)
+    GF = M*F
+    x = 0.78
+    @test GF(x) ≈ g(x) * f(x)
+
 end
