427dc7c0487d34a83ce7adab8cd61e7cc265cee9
Marcus Webb <marcusdavidwebb@gmail.com>
2021-09-21 14:43:13 +0200

    Improvements to Schrodinger.jl

--- src/Schrodinger.jl
+++ src/Schrodinger.jl
@@ -12,14 +12,7 @@ function R0_step(u::Fun, itransplan, Vmultiplier, transplan)
     return Fun(space(u),newcoeffs)
 end
 
-function plan_R1_step(u::Fun{F},ε,Δt, order = 0) where F <: Fourier
-    n = length(coefficients(u))
-    λ = (domain(u).b - domain(u).a)/(2π)
-    expD2 = [exp(-1im*ε*Δt*(λ*div(k,2))^2) for k = 1:n]
-    return expD2
-end
-
-function plan_R1_step(u::Fun,ε,Δt,order = 2)
+function plan_R1_step(u::Fun,ε,Δt,order = 0)
     n = length(coefficients(u))
     D2 = Derivative(space(u),2)[1:n,1:n]
     if order == 2
@@ -29,18 +22,18 @@ function plan_R1_step(u::Fun,ε,Δt,order = 2)
         expD2top = 12I + 6im*ε*Δt*D2 - (ε*Δt*D2)^2
         expD2bottom = 12I - 6im*ε*Δt*D2 - (ε*Δt*D2)^2
     elseif order == 6
-        expD2top = 120I + 60im*ε*Δt*D2 - 12(ε*Δt*D2)^2 - 1im(ε*Δt*D2)^3
-        expD2bottom = 120I - 60im*ε*Δt*D2 - 12(ε*Δt*D2)^2 + 1im(ε*Δt*D2)^3
+        expD2top = 120I + 60im*ε*Δt*D2 - 12*(ε*Δt*D2)^2 - 1im*(ε*Δt*D2)^3
+        expD2bottom = 120I - 60im*ε*Δt*D2 - 12*(ε*Δt*D2)^2 + 1im*(ε*Δt*D2)^3
     elseif order == 0
-        expD2top = exp(Matrix(1im*ε*Δt*D2))
+        if bandwidths(D2) == (0,0) # for example, Fourier
+            expD2top = complex(D2)
+            expD2top.data = exp.(1im*ε*Δt*expD2top.data)
+        else
+            expD2top = exp(Matrix(1im*ε*Δt*D2))
+        end
         expD2bottom = I
     end
     return (expD2bottom, expD2top)
-end
-
-function R1_step(u::Fun{F},expD2) where F <: Fourier
-    newcoeffs = expD2 .* coefficients(u)
-    return Fun(space(u),newcoeffs)
 end
 
 function R1_step(u::Fun,expD2)
@@ -48,7 +41,47 @@ function R1_step(u::Fun,expD2)
     return Fun(space(u),newcoeffs)
 end
 
-function plan_Lie_Trotter_step(u::Fun,V,ε,Δt,order=2)
+function plan_R2_step(u0::Fun,V1,V2,ε,Δt)
+    n = length(coefficients(u0))
+    D2term = (Δt^3 * ε /6 ) * Derivative(space(u0),2)[1:n,1:n]
+    itransplan = plan_itransform(space(u0),coefficients(u0))
+    transplan = plan_transform(space(u0),values(u0))
+    V1term = (Δt^3 / 12ε) * V1.(points(u0)).^2
+    V2term = V2.(points(u0))
+    return D2term, itransplan, transplan, V1term, V2term
+end
+
+function R2_step(u::Fun, D2term, itransplan, transplan, V1term, V2term, Krylov_dim=6)
+    q1 = complex(coefficients(u))
+    T = eltype(q1)
+    normq1 = norm(q1)
+    q1 = q1/normq1
+    n = length(q1)
+    m = Krylov_dim
+    Q = zeros(T,n,m) 
+    Q[:,1] = q1
+    H = zeros(T,m,n)
+        
+    for k=1:m
+        z = one(T)im * D2term * (transplan * (V2term .* (itransplan * Q[:,k])))
+        z += one(T)im * transplan * (V2term .* (itransplan * (D2term * Q[:,k])))
+        z +=  one(T)im * transplan * (V1term .* (itransplan * Q[:,k]))
+        for i=1:k
+            H[i,k] = Q[:,i]'*z
+            z = z - H[i,k]*Q[:,i]
+        end
+        if k < m
+            H[k+1,k] = norm(z)
+            if H[k+1,k] == zero(T)
+                return
+            end
+            Q[:,k+1] = z / H[k+1,k]
+        end
+    end
+    return Q, H[1:m,1:m], normq1*(Q*exp(H[1:m,1:m]))[:,1]
+end
+
+function plan_Lie_Trotter_step(u::Fun,V,ε,Δt,order=0)
     itransplan, Vmultiplier, transplan = plan_R0_step(u,V,ε,Δt)
     expD2 = plan_R1_step(u,ε,Δt,order)
     return itransplan, Vmultiplier, transplan, expD2
@@ -59,8 +92,9 @@ function Lie_Trotter_step(u::Fun, itransplan, Vmultiplier, transplan, expD2)
     return R1_step(newu,expD2)
 end
 
-function Lie_Trotter_evolve(u0::Fun,V,ε,Δt,T,order=2)
+function Lie_Trotter_evolve(u0::Fun,V,ε,Δt,T,order=0)
     M = Integer(floor(T/Δt))
+    u0 = Fun(space(u0),complex(coefficients(u0)))
     retFuns = Vector{typeof(u0)}(undef,M+1)
     retFuns[1] = u0
     itransplan, Vmultiplier, transplan, expD2 = plan_Lie_Trotter_step(u,V,ε,Δt,order)
@@ -71,7 +105,7 @@ function Lie_Trotter_evolve(u0::Fun,V,ε,Δt,T,order=2)
 end
 
 
-function plan_Strang_step(u::Fun,V,ε,Δt,order=2)
+function plan_Strang_step(u::Fun,V,ε,Δt,order=0)
     itransplan, Vmultiplier, transplan = plan_R0_step(u,V,ε,Δt/2)
     expD2 = plan_R1_step(u,ε,Δt,order)
     return itransplan, Vmultiplier, transplan, expD2
@@ -83,8 +117,9 @@ function Strang_step(u::Fun, itransplan, Vmultiplier, transplan, expD2)
     return R0_step(newu,itransplan, Vmultiplier, transplan)
 end
 
-function Strang_evolve(u0::Fun,V,ε,Δt,T,order=2)
+function Strang_evolve(u0::Fun,V,ε,Δt,T,order=0)
     M = Integer(floor(T/Δt))
+    u0 = Fun(space(u0),complex(coefficients(u0)))
     retFuns = Vector{typeof(u0)}(undef,M+1)
     retFuns[1] = u0
     itransplan, Vmultiplier, transplan, expD2 = plan_Strang_step(u0,V,ε,Δt,order)
@@ -93,6 +128,7 @@ function Strang_evolve(u0::Fun,V,ε,Δt,T,order=2)
     end
     return 0:Δt:T, retFuns
 end
+
 
 
 
