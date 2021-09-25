0e23935333d3388b15f7f3791cb5da4d746e13dc
Marcus Webb <marcusdavidwebb@gmail.com>
2021-09-25 11:25:57 +0200

    Tests for Hermite

--- test/runtests.jl
+++ test/runtests.jl
@@ -64,6 +64,23 @@ using ApproxFun
     @test v ≈ itrans*(trans*v)
     @test v ≈ trans*(itrans*v)
 
+    H = HermiteFSE()
+    f = x -> (1+x^2)*exp(-x^2 / 2)
+    F = Fun(f,H)
+    x = 0.78
+    @test F(x) ≈ f(x)
+
+    f = x -> cos(10x)*exp(-x^2)
+    F = Fun(f,H)
+    x = 0.78
+    @test F(x) ≈ f(x)
+
+    v = [0.83;0.78;0.65;1.11]
+    trans = MTFun.plan_transform(H,v)
+    itrans = MTFun.plan_itransform(H,v)
+    @test v ≈ itrans*(trans*v)
+    @test v ≈ trans*(itrans*v)
+
 end
 
 @testset "Operators" begin
@@ -112,5 +129,14 @@ end
     GF = M*F
     x = 0.78
     @test GF(x) ≈ g(x) * f(x)
+
+    H = HermiteFSE()
+    f = x -> sech(x)^2
+    fprime = x -> -2tanh(x)*sech(x)^2
+    F = Fun(f,H)
+    Fprime = differentiate(F)
+    x = 0.78
+    @test Fprime(x) ≈ fprime(x)
+    @test (Derivative(H,2)*F)(x) ≈ (differentiate(Fprime))(x)
 
 end
